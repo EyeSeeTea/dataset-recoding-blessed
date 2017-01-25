2f2f8c7268ec399e3c4c5fecc202a7ca8fc7a4b1
josemp10 <josemp10@gmail.com>
2017-01-25 23:07:56 +0100

    Removing useless files, changes in the license

--- modules/datasetRecoding/datasetRecodingController.js
+++ modules/datasetRecoding/datasetRecodingController.js
@@ -1,9 +1,9 @@
 /* 
    Copyright (c) 2016.
  
-   This file is part of Project Manager.
+   This file is part of DataSet Recoding.
  
-   Project Manager is free software: you can redistribute it and/or modify
+   DataSet Recoding is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
@@ -14,7 +14,7 @@
    GNU General Public License for more details.
  
    You should have received a copy of the GNU General Public License
-   along with Project Manager.  If not, see <http://www.gnu.org/licenses/>. */
+   along with DataSet Recoding.  If not, see <http://www.gnu.org/licenses/>. */
 
 dhisServerUtilsConfig.controller('datasetRecodingController', function($rootScope, $scope, $filter, $q, Datasets, MetaData, MetaDataAssociations, LoadForm, LoadFormValues, DataValues, CategoryCombos, DataValueSets) {
 
