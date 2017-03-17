10913ee6d8ce4ad0cd5a4eb03fb71af62e0b8b85
Arnau Sanchez <pyarnau@gmail.com>
2017-03-17 10:19:31 +0100

    add translation es/fr/ar and translate jquery calendars

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var dhisServerUtilsConfig = angular.module("dhisServerUtilsConfig", ['ngRoute', 'ngSanitize', 'Dhis2Api', 'pascalprecht.translate', 'ui.bootstrap', 'd2Menu']);
 
+var configureDhis2Calendars = function(locale) {
+    dhis2.period.format = "yyyy-mm-dd";
+    dhis2.period.calendar = $.calendars.instance('gregorian', locale);
+    dhis2.period.generator = new dhis2.period.PeriodGenerator( dhis2.period.calendar, dhis2.period.format );     
+    dhis2.period.picker = new dhis2.period.DatePicker( dhis2.period.calendar, dhis2.period.format );
+    i18n_select_option = "";
+};
+
 dhisServerUtilsConfig.config(function($routeProvider) {
     $routeProvider.when('/DatasetRecoding', {
         templateUrl: "modules/datasetRecoding/datasetRecodingView.html",
@@ -19,9 +27,12 @@ dhisServerUtilsConfig.config(function($translateProvider, urlApi) {
     });
 
     $translateProvider.registerAvailableLanguageKeys(
-        ['en'],
+        ['en', 'es', 'fr', 'ar'],
         {
             'en*': 'en',
+            'es*': 'es',
+            'fr*': 'fr',
+            'ar*': 'ar',
             '*': 'en' // must be last!
         }
     );
@@ -29,6 +40,8 @@ dhisServerUtilsConfig.config(function($translateProvider, urlApi) {
     $translateProvider.fallbackLanguage(['en']);
 
     jQuery.ajax({ url: urlApi + 'userSettings/keyUiLocale/', contentType: 'text/plain', method: 'GET', dataType: 'text', async: false }).success(function(uiLocale) {
+        configureDhis2Calendars(uiLocale);
+        
         if (uiLocale == '') {
             $translateProvider.determinePreferredLanguage();
         }
